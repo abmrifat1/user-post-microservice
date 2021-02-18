713ed20ca0a9e2d323fe8f58b33937dc1b4a9832
abmrifat1 <rifatabm1@gmail.com>
2021-02-18 17:47:09 +0600

    swagger implementation

--- postServer/src/main.ts
+++ postServer/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const options = new DocumentBuilder().setTitle('Post service')
+                  .setDescription('post micro-service api implementation.')
+                  .setVersion('1.0.0').build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(3200);
 }
 bootstrap();
